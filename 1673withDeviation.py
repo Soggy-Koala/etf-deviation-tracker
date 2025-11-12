4f709fc200c50db98acaf02465b7c6737351748d
Soggy Koala <134841180+Soggy-Koala@users.noreply.github.com>
2025-11-12 19:25:53 +0900

    Update 1673withDeviation.py

--- 1673withDeviation.py
+++ 1673withDeviation.py
@@ -7,7 +7,7 @@ import yfinance as yf
 
 def main():
     # --- 銘柄リスト ---
-    tickers = {"1672.T": "Gold"}
+    tickers = {"1673.T": "Silver"}
 
     usd_jpy = yf.Ticker("JPY=X").history(period="1d")["Close"].iloc[-1]
 
@@ -35,7 +35,7 @@ def main():
 
     # --- ファイル設定 ---
     csv_filename = "1672withDeviation.csv"
-    png_filename = "1672withDeviation.png"
+    png_filename = "1673withDeviation.png"
 
     # --- CSV追記 ---
     df_new = pd.DataFrame(data)
@@ -54,7 +54,7 @@ def main():
     color_price = "blue"
     color_deviation = "black"
 
-    df_sub = df_all[df_all["name"] == "Gold"]
+    df_sub = df_all[df_all["name"] == "Silver"]
 
     if not df_sub.empty:
         fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -75,7 +75,7 @@ def main():
 
         # 凡例
         fig.legend(loc="upper left", bbox_to_anchor=(0.1, 0.9))
-        plt.title("ETF vs NAV and Deviation (1672)")
+        plt.title("ETF vs NAV and Deviation (1673)")
         fig.tight_layout()
         plt.xticks(rotation=45, ha="right")
 
