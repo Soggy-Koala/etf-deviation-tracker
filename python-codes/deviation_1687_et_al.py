afed1b45961b9efccdfcf5a94f3c3d369c9a7bcb
Soggy Koala <134841180+Soggy-Koala@users.noreply.github.com>
2025-11-22 14:42:00 +0900

    Update deviation_1687_et_al.py

--- python-codes/deviation_1687_et_al.py
+++ python-codes/deviation_1687_et_al.py
@@ -28,7 +28,7 @@ def main():
 
     for ticker, color in colors.items():
         sub = df[df["ticker"] == ticker]
-        plt.plot(sub["timestamp"], sub["deviation_pct"], label=ticker[0:4]+names[ticker], color=color, linewidth=2)
+        plt.plot(sub["timestamp"], sub["deviation_pct"], label=ticker[0:4]+" "+names[ticker], color=color, linewidth=2)
 
     plt.title("ETF Deviation (1687 et al)", fontsize=14)
     plt.xlabel("Date")
